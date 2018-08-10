c03bf67d85caa7dce9ebeae53c620c6e0c8a83b9
fffelix <reflectedwave@gmail.com>
2018-08-10 16:38:40 +0100

    asdf

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,34 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fdubois <fdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/10 15:50:10 by fdubois           #+#    #+#             */
-/*   Updated: 2018/08/10 16:11:31 by fdubois          ###   ########.fr       */
+/*   Created: 2018/08/10 16:03:41 by fdubois           #+#    #+#             */
+/*   Updated: 2018/08/10 16:37:31 by fdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-long	ft_atol(char *str)
+char *ft_strncpy(char *dest, char *src, unsigned long long n)
 {
-	int	i;
-	long	nb;
-	long	sign;	
+	unsigned long long i;
 
 	i = 0;
-	nb = 0;
-	sign = 1;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-		i++;
-	if (str[i] == '-')
-		sign = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i] <= '9' && str[i] >= '0')
+	while (src[i] != '\0' && i < n)
 	{
-		nb = 10 * nb + (str[i] - '0');
+		dest[i] = src[i];
 		i++;
 	}
-	return (nb * sign);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fdubois <fdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/10 15:46:57 by fdubois           #+#    #+#             */
-/*   Updated: 2018/08/10 15:49:06 by fdubois          ###   ########.fr       */
+/*   Updated: 2018/08/10 16:27:33 by fdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ long		ft_atol(char *str);
 long long	ft_atoll(char *str);
 int		ft_isalpha(int c);
 int		ft_strisalpha(char *str);
+char		*ft_strcpy(char *dest, char *src);
+char		*ft_strncpy(char *dest, char *src, unsigned long long n);
 
 #endif
