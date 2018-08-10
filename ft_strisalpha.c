0eb4529b2728287cea175511f3a18ee2b1ae1af8
fffelix <reflectedwave@gmail.com>
2018-08-10 15:11:22 +0100

    j'ai changé des trucs

---
+++ ft_strisalpha.c
@@ -0,0 +1,14 @@
+int	ft_strisalpha(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] < 'A' || str[i] > 'Z') && (str[i] < 'a' || str[i] > 'z'))
+			return (0);
+		else
+			i++;
+	}
+	return (1);
+}
