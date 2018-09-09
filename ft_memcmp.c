3b21ae6ee17b5b4222ee9a5d0c15905e27f0b823
fffelix <reflectedwave@gmail.com>
2018-09-09 17:35:13 +0100

    truc

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: fdubois <fdubois@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/16 22:43:47 by fdubois           #+#    #+#             */
-/*   Updated: 2018/08/17 00:05:32 by fdubois          ###   ########.fr       */
+/*   Updated: 2018/09/04 19:42:43 by fdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ int	ft_memcmp(void const *s1, void const *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while (i < n && ((unsigned char*) s1)[i] == ((unsigned char*) s2)[i])
+	while (((unsigned char*) s1)[i] != '\0' && ((unsigned char*) s2)[i] != '\0' && i < (n - 1))
+	{
+		if (((unsigned char*) s1)[i] != ((unsigned char*) s2)[i])
+			return (((unsigned char*) s1)[i] - ((unsigned char*) s2)[i]);
 		i++;
+	}
 	return (((unsigned char*) s1)[i] - ((unsigned char*) s2)[i]);
 }
