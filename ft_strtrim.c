13f06729c91805324fbea0265cddfe549af6af91
Felix DUBOIS <fdubois@e2r5p3.42.fr>
2018-11-06 16:20:10 +0100

    tests ok pre-norme check

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,46 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdubois <fdubois@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fdubois <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/02 17:50:54 by fdubois           #+#    #+#             */
-/*   Updated: 2018/10/19 17:27:23 by fffelix          ###   ########.fr       */
+/*   Created: 2018/11/06 15:18:30 by fdubois           #+#    #+#             */
+/*   Updated: 2018/11/06 16:12:10 by fdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char	*ft_douille(char const *s)
 {
-	if (s != NULL)
-	{
-	size_t	i;
-	size_t	start;
-	size_t	end;
-	char	*str;
+		int		i;
+		size_t	start;
+		size_t	end;
+		char	*str;
 
-	i = 0;
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
-		i++;
-	start = i;
-	while (s[i] != '\0')
-	{
-		while (s[i] && s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
-			end = i++;
+		i = 0;
 		while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
 			i++;
-	}
-	if (!(str = (char*)malloc(sizeof(char) * (end - start + 2))))
-		return (NULL);
-	i = 0;
-	while ((start + i) <= end)
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-	}
-	else
-		return (NULL);
+		if (s[i] == '\0')
+			return ("\0");
+		start = i;
+		while (s[i] != '\0')
+		{
+			while (s[i] && s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+				end = i++;
+			while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+				i++;
+		}
+		if (!(str = (char*)malloc(sizeof(char) * (end - start + 2))))
+			return (NULL);
+		i = -1;
+		while ((++i + start) <= end)
+			str[i] = s[start + i];
+		str[i] = '\0';
+		return (str);
+}
+
+char	*ft_strtrim(char const *s)
+{
+		if (s == NULL)
+			return (NULL);
+		return (ft_douille(s));
 }
